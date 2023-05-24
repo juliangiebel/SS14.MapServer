8af921f8dc455ca57641f8b04d436bb6b5701ea3
juliangiebel <juliangiebel@live.de>
2023-05-25 00:08:23 +0200

    Implement github webhook handling

---
+++ SS14.MapServer/Controllers/GitHubWebhookController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Octokit;
+using SS14.MapServer.Helpers;
+
+namespace SS14.MapServer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GitHubWebhookController : ControllerBase
+{
+    private const string GithubEventHeader = "x-github-event";
+
+    private readonly IConfiguration _configuration;
+
+    public GitHubWebhookController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task Post()
+    {
+        if (!Request.Headers.TryGetValue(GithubEventHeader, out var eventName) || !await GithubWebhookHelper.VerifyWebhook(Request, _configuration))
+            return;
+
+        var json = await GithubWebhookHelper.RetrievePayload(Request);
+        var serializer = new Octokit.Internal.SimpleJsonSerializer();
+
+        switch (eventName)
+        {
+            case "push":
+                HandlePushEvent(serializer.Deserialize<PushEventPayload>(json));
+                break;
+        }
+    }
+
+    private void HandlePushEvent(PushEventPayload payload)
+    {
+        //TODO: Check if branch matches configured target branch and schedule an update maps job
+    }
+}
