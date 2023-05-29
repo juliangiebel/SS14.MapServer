521132722364ea0bf1b6c68de80481375829519d
juliangiebel <juliangiebel@live.de>
2023-05-30 00:38:16 +0200

    Work on handling push events

--- SS14.MapServer/Configuration/GitConfiguration.cs
+++ SS14.MapServer/Configuration/GitConfiguration.cs
@@ -5,6 +5,32 @@ public sealed class GitConfiguration
     public const string Name = "Git";
 
     public string RepositoryUrl { get; set; } = string.Empty;
+    
     public string TargetDirectory { get; set; } = string.Empty;
+    
     public string Branch { get; set; } = "master";
+    
+    /// <summary>
+    /// If true the map server will retrieve the list of changed maps from the github diff api.
+    /// If this is false all maps will get updated on every push.
+    /// </summary>
+    /// <remarks>
+    /// Requires the map server to be installed as a github app.
+    /// </remarks>
+    public bool RetrieveMapFilesFromDiff { get; set; } = true;
+    
+    /// <summary>
+    /// Prevent updating maps when there where any c# files changed.
+    /// </summary>
+    /// <remarks>
+    /// Requires the map server to be installed as a github app.
+    /// This setting is recommended when the map server is configured to run for PRs
+    /// as it prevents potentially malicious changes from being built and executed.
+    /// </remarks>
+    public bool DontRunWithCodeChanges { get; set; } = true;
+
+    /// <summary>
+    /// Setting this to true enables listening to the PullRequest event for putting the rendered map as a comment into the PR
+    /// </summary>
+    public bool RunOnPullRequests { get; set; } = true;
 }
--- SS14.MapServer/Controllers/GitHubWebhookController.cs
+++ SS14.MapServer/Controllers/GitHubWebhookController.cs
@@ -4,6 +4,7 @@ using Octokit;
 using SS14.GithubApiHelper.Helpers;
 using SS14.MapServer.Configuration;
 using SS14.MapServer.Helpers;
+using SS14.MapServer.Services;
 
 namespace SS14.MapServer.Controllers;
 
@@ -14,12 +15,15 @@ public class GitHubWebhookController : ControllerBase
     private const string GithubEventHeader = "x-github-event";
     private const string GitBranchRefPrefix = "refs/heads/";
 
+    private readonly GithubApiService _githubApiService;
+    
     private readonly IConfiguration _configuration;
     private readonly GitConfiguration _gitConfiguration;
 
-    public GitHubWebhookController(IConfiguration configuration)
+    public GitHubWebhookController(IConfiguration configuration, GithubApiService githubApiService)
     {
         _configuration = configuration;
+        _githubApiService = githubApiService;
         configuration.Bind(GitConfiguration.Name, _gitConfiguration);
     }
 
@@ -36,20 +40,38 @@ public class GitHubWebhookController : ControllerBase
         switch (eventName)
         {
             case "push":
-                HandlePushEvent(serializer.Deserialize<PushEventPayload>(json));
+                await HandlePushEvent(serializer.Deserialize<PatchedPushEventPayload>(json));
                 break;
         }
     }
 
-    private void HandlePushEvent(PushEventPayload payload)
+    private async Task HandlePushEvent(PatchedPushEventPayload payload)
     {
         if (!payload.Ref.Equals(GitBranchRefPrefix + _gitConfiguration.Branch))
             return;
 
-        foreach (var commit in payload.Commits)
-        {
-            
-        }
+        if (!await CheckFiles(payload))
+            return;
         //TODO: Check if branch matches configured target branch and schedule an update maps job
     }
+
+    private async Task<bool> CheckFiles(PatchedPushEventPayload payload)
+    {
+        if (!_gitConfiguration.RetrieveMapFilesFromDiff)
+            return true;
+
+
+        var files = await _githubApiService.GetChangedFilesBetweenCommits(
+            payload.Installation.Id, 
+            payload.Repository.Id, 
+            payload.Before, 
+            payload.Head);
+
+        return false;
+    }
+}
+
+public sealed class PatchedPushEventPayload : PushEventPayload
+{
+    public string Before { get; private set; }
 }
