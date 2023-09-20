75e1ab2ad49219ed46621da57b49b855214226ef
Julian Giebel <juliangiebel@live.de>
2023-09-20 23:23:58 +0200

    Replace libgit2sharp (#3)
    * Work on ssh support for repositories
    
    * Add git check to preflight checks
    Replace libgit2sharp with just running git commands using process

--- SS14.MapServer/Configuration/GitConfiguration.cs
+++ SS14.MapServer/Configuration/GitConfiguration.cs
@@ -5,9 +5,9 @@ public sealed class GitConfiguration
     public const string Name = "Git";
 
     public string RepositoryUrl { get; set; } = string.Empty;
-    
+
     public string Branch { get; set; } = "master";
-    
+
     /// <summary>
     /// If true the map server will retrieve the list of changed maps from the github diff api.
     /// If this is false all maps will get updated on every push.
@@ -24,13 +24,13 @@ public sealed class GitConfiguration
     {
         "Resources/Maps/*.yml"
     };
-    
+
     /// <summary>
     /// Glob patterns for excluding specific map files
     /// </summary>
     public List<string> MapFileExcludePatterns { get; set; } = new();
 
-    
+
     /// <summary>
     /// Prevent updating maps when there where any c# files changed.
     /// </summary>
@@ -48,9 +48,23 @@ public sealed class GitConfiguration
     {
         "**/*.cs"
     };
-    
+
     /// <summary>
     /// Setting this to true enables listening to the PullRequest event for putting the rendered map as a comment into the PR
     /// </summary>
     public bool RunOnPullRequests { get; set; } = true;
+
+    /// <summary>
+    /// The identity git will use to pull changes with. This doesn't have an effect on anything but is required
+    /// for pulling changes in some situations
+    /// </summary>
+    public GitIdentity Identity { get; set; } = new GitIdentity("ss14.mapserver", "git@mapserver.localhost");
+
+    /// <summary>
+    /// The ssh command used by git if set. Used for providing an ssh key to use.
+    /// <example>"ssh -i [path to ssh key]"</example>
+    /// </summary>
+    public string? SshCommand { get; set; }
+
+    public record GitIdentity(string Name, string Email);
 }
